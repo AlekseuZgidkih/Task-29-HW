7f873bbeef8c068e5aac5946c0f1578e91b07ed9
nordica200925 <nordica200925@gmail.com>
2023-10-24 10:07:00 +0500

    Добавьте файлы проекта.

---
+++ Task 29 HW/FineGrainedQueue.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <thread>
+#include <mutex>
+#include <iostream>
+
+struct Node
+{
+    Node(int data) : value(data), next(nullptr) { node_mutex = new std::mutex; }
+    ~Node()
+    {
+        delete node_mutex;
+    }
+    int value;
+    Node* next;
+    std::mutex* node_mutex;
+};
+
+class FineGrainedQueue
+{
+
+    std::mutex* queue_mutex = new std::mutex;
+
+public:
+    Node* head;
+    ~FineGrainedQueue();
+
+    /// @brief Добавляет элемент в начало списка. Не потокобезопасная.
+    void push_front(int data);
+    /// @brief Добавляет элемент в конец списка. Не потокобезопасная.
+    void push_back(int data);
+
+    /// @brief Вставляет элемент в середину списка. Потокобезопасная.
+    void insertIntoMiddle(int pos, int data);
+    void remove(int value);
+    void show();
+};
